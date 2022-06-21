d745e1fe04e931d90fdd888b1009286fe70fe049
Chiomasarah-UI <82842906+Chiomasarah-UI@users.noreply.github.com>
2022-06-21 13:15:55 -0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+document.write("HELLO WORLD");
+var name = ("Abana Chioma Sarah");
+var heigh = 5.6;
+var countrry = ("Nigeria");
+document.write("<br> my name is ", name);
+document.write("<br> my name full height ", heigh);
+document.write("<br> my country of origin is ", countrry);
